a50961c60457b6b5c2934b830a64101ea4e785c6
Marco Taylor-Poverello <mtaylir27@gmail.com>
2024-01-24 16:19:56 +0200

    QRCode files added

---
+++ qrcode.js
@@ -0,0 +1,33 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    let qrCodeBox = document.getElementById('qrCodeBox');
+    let qrImage = document.getElementById('qrImage');
+    let qrInput = document.getElementById('qrInput');
+    let generateButton = document.querySelector('.container .generate-button')
+    let downloadButton = document.querySelector('.container .download-button')
+
+    generateQR();
+
+    function generateQR(){
+        if(qrInput.value.length > 0){
+            let encodedInput = encodeURIComponent(qrInput.value);
+            qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" + encodedInput;
+            qrCodeBox.classList.add("show-img");
+        }
+    }
+    
+    generateButton.addEventListener("click", () => {
+        generateQR(qrInput.value);
+    })
+
+    downloadButton.addEventListener("click", async () => {
+        const response = await fetch(qrImage.src);
+        const blob = await response.blob();
+        const downloadLink = document.createElement("a");
+        downloadLink.href = URL.createObjectURL(blob);
+        downloadLink.download = "qrCode.jpg";
+        downloadLink.click();
+    })
+
+});
+
+
